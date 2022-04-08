37e00c6dc8fa75dbf47451e361799d3bceeb3e43
SporoZoite <lisavanhooydonk@gmail.com>
2022-04-08 17:06:25 +0200

    quotes modifs

--- twentytwentyone-child/audience-quotes-guide.php
+++ twentytwentyone-child/audience-quotes-guide.php
@@ -4,5 +4,13 @@
 </header>
 
 	<div class="quotes-container">
-		<?php echo get_post_meta($post->ID, 'audience_quotes', true); ?>
+
+        <div class="quotes-text">
+             <?php echo get_the_content(); ?>
+        </div>
+       
+        <div class="quotes-list">
+		     <?php echo get_post_meta($post->ID, 'audience_quotes', true); ?>
+        </div>
+
 	</div>
