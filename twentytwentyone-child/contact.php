3a0d6f51b4e9344a5549c9321c29b9b2bba5ab09
SporoZoite <lisavanhooydonk@gmail.com>
2022-04-08 22:16:14 +0200

    added page contact

---
+++ twentytwentyone-child/contact.php
@@ -0,0 +1,12 @@
+<header class="page-header alignwide">
+    <h1 class="page-title"><?php echo get_the_title(); ?></h1>
+</header>
+
+	<div class="contact-container">
+
+        <div class="contact-form">
+            <?php 	echo do_shortcode ('[contact-form-7 id="192838" title="Contact Form"]'); ?>
+        </div>
+
+    </div>
+
---
+++ twentytwentyone-child/template-contact.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Contact
+ * Template Post Type: post,page,edition-post,film
+ */
+
+get_header();
+include('contact.php');
+get_footer();
+?>
