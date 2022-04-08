d5d667276023155e70adcb67092025731be89b99
SporoZoite <lisavanhooydonk@gmail.com>
2022-04-08 16:45:37 +0200

    quotes pages

--- twentytwentyone-child/audience-quotes-guide.php
+++ twentytwentyone-child/audience-quotes-guide.php
@@ -1,6 +1,6 @@
 
 <header class="page-header alignwide">
-    <h1 class="page-title">Audience Quotes' Guide </h1>
+    <h1 class="page-title"><?php echo get_the_title(); ?></h1>
 </header>
 
 	<div class="quotes-container">
